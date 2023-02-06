8d73771082deffa298b1347d1a61252cd09ce833
Anish-ASU <anair47@asu.edu>
2023-02-06 16:14:31 -0700

    Added hide unhide button to GUI

--- src/main/java/se/bettercode/scrum/gui/ToolBar.java
+++ src/main/java/se/bettercode/scrum/gui/ToolBar.java
@@ -13,6 +13,9 @@ import javafx.scene.layout.HBox;
 
 public class ToolBar extends HBox {
 
+    private final Button toggleButton = new Button("Hide Burnup");
+
+    private final Button toggleButton2 = new Button("Unhide Burnup");
     private final Button startButton = new Button("Start Sprint");
     private final Button viewDocs = new Button("Documents");
     private ChoiceBox<String> teamChoiceBox = new ChoiceBox<>();
@@ -31,8 +34,10 @@ public class ToolBar extends HBox {
 
         startButton.setPrefSize(100, 20);
         viewDocs.setPrefSize(100, 20);
+        toggleButton.setPrefSize(120,20);
+        toggleButton2.setPrefSize(120,20);
 
-        getChildren().addAll(teamChoiceBox, backlogChoiceBox, startButton, viewDocs);
+        getChildren().addAll(teamChoiceBox, backlogChoiceBox, startButton, viewDocs, toggleButton, toggleButton2);
     }
 
     public void setStartButtonAction(EventHandler<ActionEvent> eventHandler) {
@@ -42,11 +47,21 @@ public class ToolBar extends HBox {
     public void setViewDocsButtonAction(EventHandler<ActionEvent> eventHandler) {
         viewDocs.setOnAction(eventHandler);
     }
+
+    public void setToggleButtonAction(EventHandler<ActionEvent> eventHandler) {
+        toggleButton.setOnAction(eventHandler);
+    }
+
+    public void setToggleButton2Action(EventHandler<ActionEvent> eventHandler) {
+        toggleButton2.setOnAction(eventHandler);
+    }
     public void bindRunningProperty(BooleanProperty booleanProperty) {
         teamChoiceBox.disableProperty().bind(booleanProperty);
         backlogChoiceBox.disableProperty().bind(booleanProperty);
         startButton.disableProperty().bind(booleanProperty);
         viewDocs.disableProperty().bind(booleanProperty);
+        toggleButton.disableProperty().bind(booleanProperty);
+        toggleButton2.disableProperty().bind(booleanProperty);
     }
 
     public void setTeamChoiceBoxListener(ChangeListener<String> changeListener) {
@@ -58,4 +73,8 @@ public class ToolBar extends HBox {
     }
 
 
+//    public void setToggleButtonAction(EventHandler<ActionEvent> eventHandler) {toggleButton.setOnAction(event -> {
+//        BurnupChart.this.setIsVisible(toggleButton.isSelected());
+//    });
+//    }
 }
