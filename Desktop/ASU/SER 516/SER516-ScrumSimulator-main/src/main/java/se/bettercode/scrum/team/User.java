690fa3d744e39590ba447715ea01945e3be061b6
Labdhi Sunil Jain (Student) <ljain3@sundevils.asu.edu>
2023-02-11 15:47:05 -0700

    Small change in User class

--- Desktop/ASU/SER 516/SER516-ScrumSimulator-main/src/main/java/se/bettercode/scrum/team/User.java
+++ Desktop/ASU/SER 516/SER516-ScrumSimulator-main/src/main/java/se/bettercode/scrum/team/User.java
@@ -2,31 +2,23 @@ package se.bettercode.scrum.team;
 
 public class User {
     String name;
-    int ID;
+    String email;
     String role;
+
+    public User(String name, String email, String role){
+        this.role = role;
+        this.name = name;
+        this. email = email;
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
     }
 
     public String getRole() {
         return role;
     }
 
-    public void setRole(String role){
-        this.role = role;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
+    public String getEmail(){return this.email;}
 }
 
