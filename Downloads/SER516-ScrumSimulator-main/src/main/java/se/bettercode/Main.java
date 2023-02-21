71e99687c1abf3b79e190c602226c01bb3b5b9a1
Labdhi Sunil Jain (Student) <ljain3@sundevils.asu.edu>
2023-02-21 15:28:38 -0700

    Revert "Revert "Task 108: add comment to database""
    This reverts commit b469d2dc9141d90e021d234c802e4b50189b7fe4.

---
+++ Downloads/SER516-ScrumSimulator-main/src/main/java/se/bettercode/Main.java
@@ -0,0 +1,21 @@
+package se.bettercode;
+
+import se.bettercode.scrum.Documents;
+import se.bettercode.scrum.backlog.Backlog;
+import se.bettercode.scrum.backlog.SmallBacklog;
+import se.bettercode.scrum.Sprint;
+import se.bettercode.scrum.team.CobraTeam;
+import se.bettercode.scrum.team.Team;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Starting command line app.");
+        Team team = new CobraTeam();
+        Backlog backlog = new SmallBacklog();
+        Sprint sprint = new Sprint("First sprint", 10, team, backlog);
+//        Documents documents = new Documents();
+        sprint.runSprint();
+        System.out.println("Finished command line app.");
+    }
+}
