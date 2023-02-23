a50edfd3db78d45617b83da2e5b07de523fffb3a
ljain3 <ljain3@asu.edu>
2023-02-23 13:11:40 -0700

    Task 105: Fetch data to read poject names

--- src/main/java/se/bettercode/scrum/Project.java
+++ src/main/java/se/bettercode/scrum/Project.java
@@ -1,9 +1,6 @@
 package se.bettercode.scrum;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,28 +9,36 @@ public class Project {
     public Project(){
     }
     private final String projectsFilePath = "src/main/java/se/bettercode/scrum/projectList";
-    private int projectId;
     private String projectName;
-    private String projectTeamName;
-    HashMap<Integer, ArrayList<String>> projectsMap = new HashMap<Integer, ArrayList<String>>();
+    private ArrayList<String> projectList = new ArrayList<String>();
 
+    public ArrayList<String> getProjectList(){
+        String[] token;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(projectsFilePath));
+            String line = null;
+            line = reader.readLine();
+            while (line != null){
+//                System.out.println("this is temp:" + temp);
+                projectList.add(projectName);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Sending: " + projectList);
+        return projectList;
+    }
 
-    public void addProject(int projectId, String projectName, String projectTeamName){
-        this.projectId = projectId;
+    public void setProject(String projectName){
         this.projectName = projectName;
-        this. projectTeamName = projectTeamName;
-        ArrayList<String> values = new ArrayList<>();
-        values.add(projectName);
-        values.add(projectTeamName);
-        projectsMap.put(projectId,values);
         File file = new File(projectsFilePath);
         BufferedWriter bf;
         try {
             bf = new BufferedWriter(new FileWriter(file, true));
-            for (Map.Entry<Integer, ArrayList<String>> entry : projectsMap.entrySet()){
-                bf.write(entry.getKey() + ": " + entry.getValue());
-                bf.newLine();
-            }
+            bf.write(this.projectName);
+            bf.newLine();
             bf.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
